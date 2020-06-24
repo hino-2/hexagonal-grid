e99240b923985dd3549c1a529164cc8547e2cfd0
hino-2 <sinigami0@gmail.com>
2020-06-24 17:26:11 +0500

    HexGrid is now a class 2nd canvas on top for selected hexes Mouse click highlights hex

--- src/Components/HexGrid/HexGrid.js
+++ src/Components/HexGrid/HexGrid.js
@@ -1,21 +1,66 @@
 import React, { useState, useRef, useEffect } from "react";
-import * as hexDraw from "../../Drawing/hex-grid";
+import HexGrid from "../../Drawing/hex-grid";
 import "./style.scss";
 
-const HexGrid = () => {
-	const [canvasSize, setCanvasSize] = useState({ width: 800, height: 600 });
+const HexGridContainer = () => {
+	const [canvasSize, setCanvasSize] = useState({ width: 800, height: 800 });
 	const [hexSize, setHexSize] = useState(20);
-	const [hexGridSize, setHexGridSize] = useState({ rowNum: 4, colNum: 4 });
-	const [hexOrigin, setHexOrigin] = useState({ x: 300, y: 300 });
+	const [hexGridSize, setHexGridSize] = useState({ L: 3, M: 5, N: 7 });
+	const [hexOrigin, setHexOrigin] = useState({ x: 50, y: 50 });
 	const canvasHex = useRef();
+	const canvasSelected = useRef();
+
+	let hexGridBase = new HexGrid();
+	let hexGridSelected = new HexGrid();
 
 	useEffect(() => {
 		canvasHex.current.width = canvasSize.width;
 		canvasHex.current.height = canvasSize.height;
-		hexDraw.drawHexGrid(canvasHex.current, hexGridSize, hexSize, hexOrigin, canvasSize);
+		canvasSelected.current.width = canvasSize.width;
+		canvasSelected.current.height = canvasSize.height;
+		hexGridBase.setParams({
+			canvasID: canvasHex.current,
+			canvasSize: canvasSize,
+			L: hexGridSize.L,
+			M: hexGridSize.M,
+			N: hexGridSize.N,
+			hexSize: hexSize,
+			hexOrigin: hexOrigin,
+		});
+		hexGridBase.drawHexGrid();
+
+		hexGridSelected.setParams({
+			canvasID: canvasSelected.current,
+			canvasSize: canvasSize,
+			L: hexGridSize.L,
+			M: hexGridSize.M,
+			N: hexGridSize.N,
+			hexSize: hexSize,
+			hexOrigin: hexOrigin,
+		});
+
+		// console.log(hexGridBase);
+		// console.log(hexGridSelected);
 	}, []);
 
-	return <canvas ref={canvasHex}></canvas>;
+	const handleMouse = (e) => {
+		const offsetX = e.pageX - hexGridBase.canvasRect.left - hexSize * 2;
+		const offsetY = e.pageY - hexGridBase.canvasRect.top;
+		const { q, r, s } = hexGridBase.cubeRound(hexGridBase.pixelToHex(hexGridBase.Point(offsetX, offsetY)));
+		const { x, y } = hexGridBase.hexToPixel(hexGridBase.Hex(q, r, s));
+
+		// hexGridSelected.clearCanvas();
+		hexGridSelected.drawHex({ x, y }, "#FF0000");
+
+		// console.log(q, r, s, x, y);
+	};
+
+	return (
+		<>
+			<canvas id="base" ref={canvasHex}></canvas>
+			<canvas id="selected" ref={canvasSelected} onMouseUp={handleMouse}></canvas>
+		</>
+	);
 };
 
-export default HexGrid;
+export default HexGridContainer;
--- src/Drawing/hex-grid.js
+++ src/Drawing/hex-grid.js
@@ -1,82 +1,140 @@
-const getHexCornerCoord = (center, hexSize, i) => {
-	let angle_deg = 60 * i - 30;
-	let angle_rad = (Math.PI / 180) * angle_deg;
-	let x = center.x + hexSize * Math.cos(angle_rad);
-	let y = center.y + hexSize * Math.sin(angle_rad);
+class HexGrid {
+	getHexCornerCoord = (center, i) => {
+		let angle_deg = 60 * i - 30;
+		let angle_rad = (Math.PI / 180) * angle_deg;
+		let x = center.x + this.hexSize * Math.cos(angle_rad);
+		let y = center.y + this.hexSize * Math.sin(angle_rad);
 
-	return Point(x, y);
-};
+		return this.Point(x, y);
+	};
 
-const Point = (x, y) => {
-	return { x, y };
-};
+	Point = (x, y) => {
+		return { x, y };
+	};
 
-const Hex = (q, r) => {
-	return { q, r };
-};
+	Hex = (q, r, s) => {
+		return { q, r, s };
+	};
 
-const drawLine = (canvasID, start, end) => {
-	const ctx = canvasID.getContext("2d");
-	ctx.beginPath();
-	ctx.moveTo(start.x, start.y);
-	ctx.lineTo(end.x, end.y);
-	ctx.stroke();
-	ctx.closePath();
-};
+	//  Cube = (x, y, z) => {
+	// 	return { x, y, z };
+	// };
 
-const drawHex = (canvasID, center, hexSize) => {
-	for (let i = 0; i <= 5; i++) {
-		const start = getHexCornerCoord(center, hexSize, i);
-		const end = getHexCornerCoord(center, hexSize, i + 1);
-		drawLine(canvasID, start, end);
-	}
-};
+	clearCanvas = () => {
+		const ctx = this.canvasID.getContext("2d");
+		ctx.clearRect(0, 0, this.canvasSize.width, this.canvasSize.height);
+	};
 
-/**
- * Center of hexagon
- */
-const hexToPixel = (hex, hexSize, hexOrigin) => {
-	var x = hexSize * (Math.sqrt(3) * hex.q + (Math.sqrt(3) / 2) * hex.r) + hexOrigin.x;
-	var y = hexSize * ((3 / 2) * hex.r) + hexOrigin.y;
-	return Point(x, y);
-};
+	drawLine = (start, end, color, width) => {
+		const ctx = this.canvasID.getContext("2d");
+		ctx.beginPath();
+		ctx.moveTo(start.x, start.y);
+		ctx.strokeStyle = color;
+		ctx.lineWidth = width;
+		ctx.lineTo(end.x, end.y);
+		ctx.stroke();
+		ctx.closePath();
+	};
 
-const drawHexCoordinates = (canvasID, center, hex) => {
-	const ctx = canvasID.getContext("2d");
-	ctx.fillText(hex.q, center.x - 15, center.y);
-	ctx.fillText(hex.r, center.x + 5, center.y);
-};
-
-const getHexParams = (hexSize) => {
-	const hexHeight = hexSize * 2;
-	const hexWidth = (Math.sqrt(3) / 2) * hexHeight;
-	const vertDist = (hexHeight * 3) / 4;
-	const horizDist = hexWidth;
-
-	return { hexHeight, hexWidth, vertDist, horizDist };
-};
-
-export const drawHexGrid = (canvasID, { rowNum, colNum }, hexSize, hexOrigin, canvasSize) => {
-	const { hexHeight, hexWidth, vertDist, horizDist } = getHexParams(hexSize);
-	const qLeftSide = Math.round(hexOrigin.x / hexWidth) * 2;
-	const qRigthSide = Math.round((canvasSize.width - hexOrigin.x) / hexWidth);
-	const rTopSide = Math.round(hexOrigin.y / (hexHeight / 2));
-	const rBottomSide = Math.round((canvasSize.height - hexOrigin.y) / (hexHeight / 2));
-
-	let offset = Math.floor((qLeftSide + qRigthSide) / 2); // left & right hex's offset compensation
-	for (let r = -rTopSide; r <= rBottomSide; r++) {
-		for (let q = -qLeftSide; q <= qRigthSide; q++) {
-			let center = hexToPixel(Hex(q + offset, r), hexSize, hexOrigin);
-			if (
-				center.x > hexWidth / 2 &&
-				center.x < canvasSize.width - hexWidth / 2 &&
-				center.y > hexHeight / 2 &&
-				center.y < canvasSize.height - hexHeight / 2
-			) {
-				drawHex(canvasID, center, hexSize);
-				drawHexCoordinates(canvasID, center, Hex(q, r));
-			}
+	drawHex = (center, color) => {
+		// console.log(this.canvasID);
+		for (let i = 0; i <= 5; i++) {
+			const start = this.getHexCornerCoord(center, i);
+			const end = this.getHexCornerCoord(center, i + 1);
+			this.drawLine(start, end, color);
 		}
-		offset -= Math.abs(r) % 2 === 0 ? 1 : 0;
-	}
-};
+	};
+
+	cubeRound = (cube) => {
+		let rx = Math.round(cube.q);
+		let ry = Math.round(cube.r);
+		let rz = Math.round(cube.s);
+
+		let x_diff = Math.abs(rx - cube.q);
+		let y_diff = Math.abs(ry - cube.r);
+		let z_diff = Math.abs(rz - cube.s);
+
+		if (x_diff > y_diff && x_diff > z_diff) {
+			rx = -ry - rz;
+		} else if (y_diff > z_diff) {
+			ry = -rx - rz;
+		} else rz = -rx - ry;
+
+		return this.Hex(rx, ry, rz);
+	};
+
+	/**
+	 * Center of hexagon
+	 */
+	hexToPixel = (hex) => {
+		var x =
+			this.hexSize * (Math.sqrt(3) * hex.q + (Math.sqrt(3) / 2) * hex.r) +
+			this.hexOrigin.x +
+			((this.L % 2) + 1) * this.hexSize;
+		var y = this.hexSize * ((3 / 2) * hex.r) + this.hexOrigin.y;
+		return this.Point(x, y);
+	};
+
+	/**
+	 * Axial coords of hexagon by pixel coords
+	 */
+	pixelToHex = ({ x, y }) => {
+		let q = ((Math.sqrt(3) / 3) * (x - this.hexOrigin.x) - (1 / 3) * (y - this.hexOrigin.y)) / this.hexSize;
+		let r = ((2 / 3) * (y - this.hexOrigin.y)) / this.hexSize;
+		return this.Hex(q, r, -q - r);
+	};
+
+	drawHexCoordinates = (center, hex) => {
+		const ctx = this.canvasID.getContext("2d");
+		ctx.fillText(hex.q, center.x + 5, center.y - 5);
+		ctx.fillText(hex.r, center.x - 9, center.y - 5);
+		ctx.fillText(hex.s, center.x - 5, center.y + 10);
+	};
+
+	// const getHexParams = () => {
+	// 	const hexHeight = this.hexSize * 2;
+	// 	const hexWidth = (Math.sqrt(3) / 2) * hexHeight;
+	// 	const vertDist = (hexHeight * 3) / 4;
+	// 	const horizDist = hexWidth;
+
+	// 	return { hexHeight, hexWidth, vertDist, horizDist };
+	// };
+
+	drawHexGrid = () => {
+		let offsetNum = 0;
+		let offsetQ = 0;
+		for (let r = 0; r < this.L + this.M - 1; r++) {
+			for (let q = 0; q < this.N + offsetNum; q++) {
+				let center = this.hexToPixel(this.Hex(q + offsetQ, r));
+				this.drawHex(center);
+				this.drawHexCoordinates(center, this.Hex(q + offsetQ, r, -q - r));
+			}
+			if (r < this.L - 1) {
+				offsetNum++;
+				offsetQ--;
+			}
+			if (r >= this.M - 1) offsetNum--;
+		}
+	};
+
+	setParams = ({ canvasID, L, M, N, hexSize, hexOrigin, canvasSize }) => {
+		this.canvasID = canvasID;
+		this.canvasSize = canvasSize;
+		this.L = L;
+		this.M = M;
+		this.N = N;
+		this.hexSize = hexSize;
+		this.hexOrigin = hexOrigin;
+		this.canvasRect = canvasID.getBoundingClientRect();
+	};
+
+	canvasID = null;
+	canvasRect = {};
+	L = 0;
+	M = 0;
+	N = 0;
+	hexOrigin = { x: 0, y: 0 };
+	hexSize = 0;
+}
+
+export default HexGrid;
